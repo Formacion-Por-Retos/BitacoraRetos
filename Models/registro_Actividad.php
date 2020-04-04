fc8fd9df4bd7b6e832d7703e660b5ccfd670340d
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-04-04 13:30:35 -0500

    Implementación de Home.

---
+++ Models/registro_Actividad.php
@@ -0,0 +1,17 @@
+<?php
+require 'conexion_db.php';
+session_start();
+$id = $_SESSION['id'];
+$nombre = $_POST['nombre'];
+$datepicker = $_POST['datepicker'];
+$proyecto = $_POST['proyecto'];
+$descripcion = $_POST['descripcion'];
+
+$query = "insert into Actividades (nombre, fecha, usuario, proyecto, descripcion) values ('$nombre','$datepicker','$id','$proyecto','$descripcion')";
+
+if (mysqli_query($conexion, $query)) {
+    echo "Se ha registrado la actividad.";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+}
+mysqli_close($conexion);
