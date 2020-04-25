a62705330627ab16e7341cddd7ae37ff298b1220
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-04-24 22:24:41 -0500

    Actualizaciones de tablas.

--- Models/eliminar_actividad.php
+++ Models/eliminar_actividad.php
@@ -2,7 +2,7 @@
 if(isset($_GET["id"])){
 	include "../conexion_db.php";
 	$con  = connect();
-	$sql = "delete from Users_Actividades where Users_id=".$_GET["id"];
+	$sql = "delete from Users_Actividades where Actividades_id=".$_GET["id"];
 	$con->query($sql);
 	$sql = "delete from Actividades where id=".$_GET["id"];
 	$con->query($sql);
