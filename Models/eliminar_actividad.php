91a8645dc7b10e1fc34d9ecbf0c190857736f2a4
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-04-24 21:49:05 -0500

    Actualización menor.

--- Models/eliminar_actividad.php
+++ Models/eliminar_actividad.php
@@ -2,10 +2,20 @@
 
 include "../conexion_db.php";
 if(isset($_GET["id"])){
+
+
 	$con  = connect();
-	$sql = "delete from Users_Actividades where Users_id=".$_GET["id"];
+
+	$sql = "delete from Users_Actividades where Actividades_id=".$_GET["id"];
+
 	$con->query($sql);
+
 	$sql = "delete from Actividades where id=".$_GET["id"];
 	$con->query($sql);
-	header("Location: ../Views/home_activities.php");}
+
+
+
+
+
+}
 
